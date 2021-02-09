c5400b3ea5d68bc146decd707aebd05dda78ec0f
vidhishweta01 <vidhishweta01@gmail.com>
2021-02-09 17:37:06 +0530

    fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,15 +1,16 @@
 def bubble_sort(array)
-  n = array.length 
+  n = array.length
   i = 0
-  j = 0  
+  j = 0
   while j < n
-    for i in 0...n-j-1
-      if array[i] > array[i + 1]
-        array[i], array[i + 1] = array[i + 1], array[i]
+    for i in 0...n - j - 1
+      if array[i]>array[i + 1]
+        array[i], array[i+1] = array[i + 1], array[i]
       end
     end
-        j = j + 1
+        j+=1
   end
   array
-end 
+end
+
 print bubble_sort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
